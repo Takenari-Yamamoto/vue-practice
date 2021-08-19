3712627e9c46ad7ff7d7f3ecdd4ead9aa4bcb009
Takenari-Yamamoto <tktkymnr1216@gmail.com>
2021-08-20 00:02:57 +0900

    vue router導入

---
+++ src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <button @click="toUsers">Usersのページに移動</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  methods: {
+    toUsers() {
+      this.$router.push({ path: "users" });
+    }
+  }
+};
+</script>
---
+++ src/pages/Users.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Users</h1>
+    <p>User NO. {{ $route.params.id }}</p>
+  </div>
+</template>
